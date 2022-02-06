5911fdd327cfc3806a35af2af749f80c20b8bb33
Brr1-99 <93494400+Brr1-99@users.noreply.github.com>
2022-02-06 18:04:42 +0100

    finished project

--- interface.py
+++ interface.py
@@ -11,6 +11,6 @@ def product(name: str, url: str, price: int):
 		'price': price
 	}
 
-WISH_LIST.append(product(name='Teclado Mecánico', 
-	url='https://www.amazon.es/MARSGAMING-MK5BRES-Teclado-Mec%C3%A1nico-Espa%C3%B1ol/dp/B08BWK1RGL/ref=sr_1_1_sspa?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2L30FF0DRH6PU&keywords=teclado+mecanico&qid=1644144266&sprefix=teclado+me%2Caps%2C104&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFXSENENFMzNFZEV1YmZW5jcnlwdGVkSWQ9QTEwMTc2MTcxMTRFU0s0VVlMS1ZGJmVuY3J5cHRlZEFkSWQ9QTA5MTA5NTIyRFY0RlJBNDhGR1M5JndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==',
-	price=50))
+WISH_LIST.append(product(name='Name of your product', 
+	url='Url in Amazon',
+	price='Desired price to look for'))
--- main.py
+++ main.py
@@ -11,6 +11,8 @@ headers=({
 
 def checkPrice():
 
+	data = []
+
 	# Looping through all your wished products
 	for product in WISH_LIST:
 		
@@ -25,11 +27,43 @@ def checkPrice():
 		#Fetch for its current price
 		price = float(soup.find('span', class_='a-offscreen').get_text().strip()[:-1].replace(',','.'))
 
+
 		#Checking if it suits your budget  
 		if price < product['price']:
+			product['price'] = price
+			data.append(product)
+	
+	if len(data) > 0 :
+	   sendMail(data)
 
-			# If it does, it will send you an email
-			sendMail(product['name'], title, price)
+def sendMail(products):
 
-def sendMail(name, title, price):
-	return name, title, price
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	server.ehlo()
+	server.starttls()
+	server.ehlo()
+
+	server.login('your@gmail.com', 'password')
+
+	subject = 'Some of your items have reached your price!'
+
+	body = 'Check the prices on this products: \n\n'
+
+	for product in products:
+		url = product['url']
+		name = product['name']
+		price = product['price']
+		body+= f'"{name}" ahora cuesta {price} €. Haz click en el link para comprarlo.{url}\n'
+
+	msg = f'Subject: {subject} \n\n {body}'
+
+	server.sendmail('your@gmail.com','your@gmail.com', msg.encode('utf-8').strip() )
+
+	print('Email Sent!')
+
+	server.quit()
+
+checkPrice()
+
+
+
